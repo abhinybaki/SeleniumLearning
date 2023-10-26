d70fbb54a60a2b9349d3bef6edad8dbe6d90e7f1
Baki <abaki865@cable.comcast.com>
2023-10-26 15:53:14 -0500

    updating commit

---
+++ src/main/java/seleniumsessions/ReverseAString.java
@@ -0,0 +1,15 @@
+package seleniumsessions;
+
+public class ReverseAString {
+
+	public static void main(String[] args) {
+
+		String str = "Abhiny Baki";
+		String reverse = new StringBuilder(str).toString().toLowerCase();
+		System.out.println("My reversed string is : " + reverse);
+		
+		
+		
+	}
+
+}
